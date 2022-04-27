849c6e58e96dfc91b8216816e276011038542955
BeomSoo-Ann <bumsuan@gmail.com>
2022-04-27 17:24:24 +0900

    Set Category page

---
+++ _pages/category.md
@@ -0,0 +1,5 @@
+---
+title: Category
+permalink: /categories/
+layout: single
+---
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Github blog의 첫 시작",
+        "excerpt":"우주과학과에 들어와서, 별로 흥미를 느끼지 못하고 술만 마셨지만   코딩을 접하며 코딩 뿐만 아니라 천문학, 우주과학에도 관심을 가지기 시작했어요   이 글, 이 블로그와 함께 첫 발을 내딛어봅니다!  ","categories": ["Blog"],
+        "tags": ["Blog"],
+        "url": "/blog/first-post/",
+        "teaser": null
+      }]
