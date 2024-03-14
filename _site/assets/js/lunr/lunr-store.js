05eb494e761f8defb0ca86d827d3f0b9931574c5
BeomSoo_Ann <68950105+BeomSoo-Ann@users.noreply.github.com>
2024-03-14 15:43:17 +0900

    check local

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,20 +1,8 @@
 var store = [{
-        "title": "Github blog의 첫 시작",
-        "excerpt":"우주과학과에 들어와서 별로 흥미를 느끼지 못하고 열심히 놀기만 했다.   코딩을 접하며 코딩 뿐만 아니라 천문학, 우주과학에도 관심을 가지기 시작했다!   이 글, 이 블로그와 함께 첫 발을 내딛어보자!  ","categories": ["Blog"],
-        "tags": [],
-        "url": "/blog/first-post/",
-        "teaser": null
-      },{
         "title": "천문학개론2 - 태양 자전주기 구하기",
         "excerpt":"천문학개론2 강의에서 진행했던 실습을 개발자의 눈으로 복습해보자! 원래의 실습에서는 태양의 자전 주기를 구하기 위한 계산식에 중점을 두지만, 이번 복습에서는 파이썬 코드를 통해 데이터를 읽고 처리하는 것에 중점을 두고자 한다. 1. 개요 Helioviewer에서 PROBA-2 위성의 태양 관측 데이터를 받아 파이썬 코드를 통해 처리한다. 처리한 데이터로 태양 표면 활동지역의 이동 속도를 구하고,...","categories": ["Astronomy"],
         "tags": [],
         "url": "/astronomy/practice-day1/",
-        "teaser": null
-      },{
-        "title": "유럽 여행 - 런던 PART1",
-        "excerpt":"2022년 드디어 코로나가 끝을 보이기 시작했다. 그리고 나의 공익 생활도 끝이 보이기 시작했다..! 복무가 끝나면 가장 먼저 하고 싶은 것이 여행이다. 연말 여행을 떠나기 전에 20년도 떠났던 유럽 여행을 되돌아보려고 한다. 시작 19년도 2학기는 개인적으로 힘든 일이 너무 많았다. 자취방에서 술과 함께 나뒹굴다가 갑자기 결단을 내렸다. 이렇게 살면 안되겠다 싶어...","categories": ["Travel"],
-        "tags": [],
-        "url": "/travel/London/",
         "teaser": null
       },{
         "title": "천문학개론2 - 보이저 1호 데이터 처리하기",
@@ -27,11 +15,5 @@ var store = [{
         "excerpt":"7월 12일, James Webb Space Telescope(JWST)가 첫 관측 데이터를 지구로 보냈다. 지금까지 천문우주과학에 큰 기여를 했던 Hubble Space Telescope (HST)와 JWST의 데이터를 사용하여 과거의 데이터를 보정해주는 딥러닝 알고리즘을 개발해보자. 1. BACKGROUND James Webb Space Telescope(JWST)는 Hubble Space Telescope (HST)의 뒤를 잇는 우주 천체 망원경으로 네 가지 주요 임무를 가지고 현재...","categories": ["Project"],
         "tags": [],
         "url": "/project/Research-Proposal/",
-        "teaser": null
-      },{
-        "title": "아프다...",
-        "excerpt":"두 달 넘게 블로그 업데이트를 거의 하지 않고 있었다… 건강이 많이 안좋아졌다. 거의 두 달 동안 하루에 2시간 이상 잠을 잔 적이 없다… 완치된 줄 알았던 아토피가 최근에 재발했다. 간지러움에 밤에 한숨도 잘 수 없었다. 22년 그럴싸한 계획을 많이 세웠고 6월, 7월까지는 지키는 듯 싶었으나 몸이 안좋아지니 할 수도, 하고싶지도...","categories": ["Blog"],
-        "tags": [],
-        "url": "/blog/Restart/",
         "teaser": null
       }]
